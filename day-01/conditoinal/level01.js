73f6fe35b9539c694a56d89d8a6a625bd0e17e67
MehediHasan06 <mehedihasanmasud06@gmail.com>
2023-02-09 20:45:07 +0600

    conditional level01 completed

---
+++ day-01/conditoinal/level01.js
@@ -0,0 +1,9 @@
+let age = 12;
+age >= 18 
+  ? console.log("You're old enough to drive.")
+  : console.log(`You are left with ${18-age} years to drive.`);
+
+let number = 3;
+number % 2 == 0 
+  ? console.log(`${number} is even.`)
+  : console.log(`${number} is odd.`);
