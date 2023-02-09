8d83b7e3400f2285115651f5e58af6045392e6ba
MehediHasan06 <mehedihasanmasud06@gmail.com>
2023-02-09 20:55:35 +0600

    conditional level02 completed

---
+++ day-01/conditoinal/level02.js
@@ -0,0 +1,23 @@
+let score = 68;
+switch (true) {
+  case score>=80 && score<=100:
+    console.log("A");
+    break;
+  case score>60 && score<80:
+    console.log("B")
+    break;
+  default:
+    console.log("Fail !!!");
+};
+
+let weekday = "Friday";
+switch (weekday) {
+  case "Saturday":
+    console.log("Weekend");
+    break;
+  case "Sunday":
+    console.log("Weekend");
+    break;
+  default:
+    console.log("Weekday !!!");
+};
